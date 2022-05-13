e3537481d53e3e85682d83bc07d797829da3087a
Hebert Costa <hjcosta.dev@gmail.com>
2022-05-13 12:12:41 -0300

    feat(tp3): analyse petrucchi's example

--- tp3/example/server_petrucchi.c
+++ tp3/example/server_petrucchi.c
@@ -6,97 +6,128 @@
 
 typedef struct{
     char id[2];
-    bool sensores[4];
-} Equipamento;
+    bool sensors[4];
+} Equipment;
 
-// Inicializa o equipamento com todos os sensores inativos
-Equipamento InicializaEquipamento(char* id){
-    Equipamento E;
+/**
+ * @brief Initialize new equipment with all sensors deactivated.
+ * 
+ * @param id  TODO...
+ * @return Equipment 
+ */
+Equipment initEquipment(char* id){
+    Equipment E;
     E.id[0] = id[0];
     E.id[1] = id[1];
-    E.sensores[0] = false;
-    E.sensores[1] = false;
-    E.sensores[2] = false;
-    E.sensores[3] = false;
+    E.sensors[0] = false;
+    E.sensors[1] = false;
+    E.sensors[2] = false;
+    E.sensors[3] = false;
     return E;
 }
 
-/* Realiza a instalação do sensor, com os seguintes valores de retorno:
-    0 = O sensor estava desinstalado e foi instalado com sucesso
-    1 = O sensor já estava instalado
-    2 = O sensor não foi instalado pois o máximo de sensores já está em uso
-*/
-int InstalaSensor(Equipamento *e, int id_sensor, int *alarmes_ativos){
-    if (*alarmes_ativos >= 15)
+/**
+ * @brief Activate na equipment sensor.
+ * 
+ * Returns:
+ * 0 = Success;
+ * 1 = Error: Sensor already installed;
+ * 2 = Error: Max sensors achieved; TODO: check if we're really gonna do it this way
+ * 
+ * @param e   TODO...
+ * @param id   TODO...
+ * @param activeSensorsCount   TODO...
+ * @return int 
+ */
+int addSensor(Equipment *equipment, int id, int *activeSensorsCount){
+    if (*activeSensorsCount >= 15)
         return 2;
-    else if (e->sensores[id_sensor-1] == 0){
-        e->sensores[id_sensor-1] = 1;
-        *alarmes_ativos += 1;
+    else if (equipment->sensors[id-1] == 0){
+        equipment->sensors[id-1] = 1;
+        *activeSensorsCount += 1;
         return 0;
     }
     else
         return 1;
 }
 
-/* Realiza a desinstalação do sensor, com os seguintes valores de retorno:
-    0 = O sensor estava instalado e foi desinstalado com sucesso
-    1 = O sensor já estava desinstalado
-*/
-int DesinstalaSensor(Equipamento *e, int id_sensor, int *alarmes_ativos){
-    if (e->sensores[id_sensor-1] == 0)
+/**
+ * @brief Remove one sensor from an equipment.
+ * 
+ * Returns:
+ * 0 = Success;
+ * 1 = Error: Sensor already installed; TODO: check if this is necessary...
+ * 
+ * @param e 
+ * @param id 
+ * @param activeSensorsCount ; TODO: check if we're really gonna do it this way
+ * @return int 
+ */
+int removeSensor(Equipment *equipment, int id, int *activeSensorsCount){
+    if (equipment->sensors[id-1] == 0)
         return 1;
     else {
-        e->sensores[id_sensor-1] = 0;
-        *alarmes_ativos -= 1;
+        equipment->sensors[id-1] = 0;
+        *activeSensorsCount -= 1;
         return 0;
     }
 }
 
-const char* ConsultaEquipamento(Equipamento e){
-    char *retorno;
-    bool tem_sensor = false;
+/**
+ * @brief TODO...
+ * 
+ * @param e 
+ * @return const char* 
+ */
+const char* listSensors(Equipment equipment){
+    char *resultList;
+    bool hasSensor = false;
 
-    if (e.sensores[0] == true){
-        strcat(retorno, "01 ");
-        tem_sensor = true;
+    if (equipment.sensors[0] == true){
+        strcat(resultList, "01 ");
+        hasSensor = true;
     }
-    if (e.sensores[1] == true){
-        strcat(retorno, "02 ");
-        tem_sensor = true;
+    if (equipment.sensors[1] == true){
+        strcat(resultList, "02 ");
+        hasSensor = true;
     }
-    if (e.sensores[2] == true){
-        strcat(retorno, "03 ");
-        tem_sensor = true;
+    if (equipment.sensors[2] == true){
+        strcat(resultList, "03 ");
+        hasSensor = true;
     }
-    if (e.sensores[3] == true){
-        strcat(retorno, "04");
-        tem_sensor = true;
+    if (equipment.sensors[3] == true){
+        strcat(resultList, "04");
+        hasSensor = true;
     }
-    if (tem_sensor == false){
+    if (hasSensor == false){
         return "none";
     }
 
-    return retorno;
+    return resultList;
 }
 
 
-// Está dando erro, não consegui consertar
-const char* ConsultaSensor(Equipamento e, int *id_sensor){
+/**
+ * TODO: 2022-05-13 - Checar isso...
+ * TODO: 2022-05-13 - ADD Descricao
+ */
+const char* readSensor(Equipment equipment, int *id){
+    
     char *retorno_tem, *retorno_nao_tem, *temp;
     int i, r;
-    int tamanho_vetor = sizeof(id_sensor)/sizeof((id_sensor)[0]);
+    int tamanho_vetor = sizeof(id)/sizeof((id)[0]);
     bool falta_sensor = false;
 
     strcat(retorno_nao_tem, "sensor(s) ");
 
     for (i=0; i<tamanho_vetor; i++){
-        if (e.sensores[id_sensor[i] - 1] == true){
+        if (equipment.sensors[id[i] - 1] == true){
             r = rand() % 100;
             sprintf(temp, "%d", r);
             strcat(retorno_tem, temp);
         }
         else {
-            sprintf(temp, "%d", id_sensor[i]);
+            sprintf(temp, "%d", id[i]);
             strcat(retorno_nao_tem, " 0");
             strcat(retorno_nao_tem, temp);
             falta_sensor = true;
@@ -116,35 +147,35 @@ int main(){
     // Seed para números aleatórios
     srand(time(NULL));
 
-    // Inicializando os equipamentos e o contador de sensores ativos
+    // Inicializando os equipamentos e o contador de sensors ativos
     int alarmes_ativos = 0;
-    Equipamento Equipamentos[4];
-    Equipamentos[0] = InicializaEquipamento("01");
-    Equipamentos[1] = InicializaEquipamento("02");
-    Equipamentos[2] = InicializaEquipamento("03");
-    Equipamentos[3] = InicializaEquipamento("04");
+    Equipment Equipamentos[4];
+    Equipamentos[0] = initEquipment("01");
+    Equipamentos[1] = initEquipment("02");
+    Equipamentos[2] = initEquipment("03");
+    Equipamentos[3] = initEquipment("04");
 
-    InstalaSensor(&Equipamentos[0], 1, &alarmes_ativos);
+    addSensor(&Equipamentos[0], 1, &alarmes_ativos);
     printf("%s\n", Equipamentos[0].id);
-    printf("%d\n", Equipamentos[0].sensores[0]);
+    printf("%d\n", Equipamentos[0].sensors[0]);
     printf("%d\n", alarmes_ativos);
 
     printf("-----------------\n");
 
-    printf("%s\n", ConsultaEquipamento(Equipamentos[0]));
-    printf("%s\n", ConsultaEquipamento(Equipamentos[1]));
+    printf("%s\n", listSensors(Equipamentos[0]));
+    printf("%s\n", listSensors(Equipamentos[1]));
 
     printf("-----------------\n");
 
     int teste[2] = {1, 2};
-    printf("%s\n", ConsultaSensor(Equipamentos[0], teste));
-    printf("%s\n", ConsultaSensor(Equipamentos[1], teste));
+    printf("%s\n", readSensor(Equipamentos[0], teste));
+    printf("%s\n", readSensor(Equipamentos[1], teste));
 
     printf("-----------------\n");
 
-    DesinstalaSensor(&Equipamentos[0], 1, &alarmes_ativos);
+    removeSensor(&Equipamentos[0], 1, &alarmes_ativos);
     printf("%s\n", Equipamentos[0].id);
-    printf("%d\n", Equipamentos[0].sensores[0]);
+    printf("%d\n", Equipamentos[0].sensors[0]);
     printf("%d\n", alarmes_ativos);
 
     return 0;
